3a065fd8a65814bd803264923391ec703e264161
jhsun <wngus2991@gmail.com>
2021-05-20 14:34:15 +0900

    NameActivity Using GuideLines

--- app/src/main/java/com/example/lotto/MainActivity.kt
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -4,14 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<View>(R.id.ConstellationActivity).setOnClickListener{
-            startActivity(Intent(this, ConstellationActivity::class.java))
+        /*findViewById<View>(R.id.cardConstellation).setOnClickListener{
+            startActivity(Intent(this, ConstellationActivity::class.java))*/
+
+        val cardConstellation = findViewById<CardView>(R.id.cardConstellation)
+        cardConstellation.setOnClickListener{
+            startActivity(Intent(this@MainActivity, ConstellationActivity::class.java))
+
         }
 
     }
